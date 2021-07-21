2d9420cfeb8daee06ae46b0cce1603f640448e4f
jarocode <okolieuchenna890@gmail.com>
2021-07-21 06:58:13 +0100

    offline support and caching

--- app/api/client.js
+++ app/api/client.js
@@ -1,7 +1,27 @@
 import {create} from 'apisauce';
 
+import cache from '../utility/cache';
+
 const apiClient = create({
     baseURL: 'http://192.168.43.123:9000/api'
-})
+});
+
+const get = apiClient.get;
+
+apiClient.get = async (url, params, axiosConfig) => {
+    try {
+        const response  = await get(url, params, axiosConfig);expo
+        if(response.ok){
+            cache.store(url, response.data);
+            return response;
+        }
+
+        const data = await cache.get(url);
+        return data ? {ok : true, data} : response;
+    
+    } catch (error) {
+        console.log(error);
+    }
+}
 
 export default apiClient;
